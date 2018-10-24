9ff1c6b8bedcefc0c82c17a80a6810e873b5f370
tothjulia <43248964+tothjulia@users.noreply.github.com>
2018-10-24 10:23:26 +0200

    Final code
    Here you can find the final code that prices the call and put options based on Arma(1,1)-Garch(1,1) model

--- data_forecasting_option_pricing.R
+++ data_forecasting_option_pricing.R
@@ -1,53 +1,75 @@
-install.packages("tidyquant")
 library(tidyverse)
 library(tidyquant)
-library(magrittr)
 library(quantmod)
-library(plyr)
-library(dplyr)
-install.packages("rugarch")
 library(rugarch)
 
-start <- as.Date("2014-10-01")
-end <- as.Date("2018-10-01")
 
+#downloading the relevant data from yahoo
 yahoo <- function(ticker, start, end) {
   tq_get(ticker, get = "stock.prices", from = start, to = end) %>%
     select(date, adjusted) %>% 
     mutate( LogPrice = log(adjusted), LogReturn = c(0,diff(LogPrice)))
 }  
-acf(yahoo("AAPL", start, end)[,4])
-pacf(yahoo("AAPL", start, end)[,4], lag.max = 100)
-aapl_data = yahoo("AAPL", start, end)
 
-#Plain GARCH model, underlying is arma(1,1)
-model=ugarchspec (
-  variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
-  mean.model = list(armaOrder = c(1, 1)),
-  distribution.model = "norm"
-)
-
-SimEndReturns <- function(stock_data, nsim = 63, msim = 1000){
+#Plain GARCH model, underlying is arma(p,q)
+underlying_arma <- function(p,q){  
+  ugarchspec (
+      variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
+      mean.model = list(armaOrder = c(p, q)),
+      distribution.model = "norm"
+    )
+}
+SimEndReturns <- function(ticker, start = "2014-10-01", end = "2018-10-01", p = 1, q = 1, nsim = 63, msim = 1000){
   #fitting garch to the stock return data
-  modelfit = ugarchfit(data=data.frame(stock_data)[, 4], spec = model, solver = "solnp", 
+  stock_data <- yahoo(ticker, start, end)
+  modelfit <-  ugarchfit(data=data.frame(stock_data)[, 4], spec = underlying_arma( p, q), solver = "solnp", 
                        fit.control=list(scale=1),out.sample=2 , solver.control=list(trace=1)) 
   #simulating 1000 forecast horizons for one quarter, 63 days
-  modelsim = ugarchsim(fit = modelfit, n.sim = nsim, n.start = 0, m.sim = msim, 
+  modelsim <- ugarchsim(fit = modelfit, n.sim = nsim, n.start = 0, m.sim = msim, 
                        startMethod = "sample")
-  #the cumulated return, which is just a sum
-  endprice_vector = exp(colSums(modelsim@simulation$seriesSim))
+  #the cumulated return for the end of the 63. day, simulated 1000 times
+  endprice_vector <- exp(colSums(modelsim@simulation$seriesSim))
 }
-#testing out the function
-end_returns <- SimEndReturns(aapl_data)
 
 #pricing the option based on E(max(S1 - K, 0))
-EvOptionPricer <- function(stock_data, K, days_left = 63, msim = 1000){
-  current_price = as.numeric(stock_data[dim(stock_data)[1], 2])
-  end_returns <- SimEndReturns(stock_data, nsim = days_left, msim = msim)
-  end_prices = end_returns * current_price
-  dif <- end_prices - K
-  dif[dif < 0] = 0
-  mean(dif)
+EvOptionPricer <- function(ticker, start = "2014-10-01", end = "2018-10-01", p = 1, q = 1, days_left = 63, msim = 1000, opt){
+  stock_data <- yahoo(ticker, start, end)
+  current_price = tail(stock_data, n = 1)[2]
+  end_returns <- SimEndReturns(ticker, start, end,  p, q, nsim = days_left, msim = msim)
+  end_prices = end_returns * as.numeric(current_price)
+  dif <- end_prices - as.numeric(current_price)
+  if (opt == "call") dif[dif < 0] = 0
+  if (opt == "put") {dif[dif > 0] = 0; dif = -1 * dif}
+  return (round(mean(dif), 2))
 }
 
-EvOptionPricer(aapl_data, 225)
+#historical volatility for the BS model
+hist_vol <- function(ts, days_in_y = 250){
+  return(sqrt(var(ts) * days_in_y))
+}
+
+#pricing the option based on the BS model
+BsOptPricer <- function(ticker, start = "2014-10-01", end = "2018-10-01", days_left = 63, r, opt){
+  stock_data <- yahoo(ticker, start, end)
+  rets <- stock_data[,4]
+  vol <- hist_vol(rets)
+  S = as.numeric(tail(stock_data, n = 1)[2])
+  K = S
+  r = log(as.numeric( tail( getSymbols('DGS3MO',src = 'FRED', from = "2018-10-01", to = Sys.Date(), auto.assign = F), n = 1))/100 + 1)
+  d1 <- (log(S / K) + (r + vol ^ 2 / 2) * days_left / 250) /
+    (vol * sqrt(days_left / 250))
+  d2 <- d1 - vol * sqrt(days_left / 250)
+  if (opt == "call"){
+    price <- pnorm(d1) * S - pnorm(d2) * K * exp(-r * days_left / 250)}
+  else {
+    price <- pnorm(-1*d2) * K * exp(-r * days_left / 250) - pnorm(-1*d1) * S}
+  return(as.numeric(price))
+}
+
+
+tickers <- c("AMD", "AMZN", "AAPL", "BAC", "MSFT")
+tibble(ticker = tickers) %>% mutate( EmpiricalCallPrice = Vectorize(EvOptionPricer)(ticker = ticker, opt = "call")) %>%
+  mutate( EmpiricalPutPrice = Vectorize(EvOptionPricer)(ticker = ticker, opt = "put")) %>%
+  mutate( BsCallPrice = Vectorize(BsOptPricer)(ticker = ticker, opt = "call")) %>%
+  mutate( BsPutPrice = Vectorize(BsOptPricer)(ticker = ticker, opt = "put"))
+
